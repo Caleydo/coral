d79fde53ff3c073e82ad04c8e6e39eab50a1e96b
Klaus Eckelt <Klaus.Eckelt@jku.at>
2021-11-10 15:40:30 +0100

    remove old files

--- dist/initialize.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Caleydo Team on 31.08.2016.
- */
-import { App } from './app';
-const APP_NAME = 'Coral';
-document.title = APP_NAME;
-const tdpApp = new App(APP_NAME); // assign to variable to avoid linting errors
-//# sourceMappingURL=initialize.js.map
