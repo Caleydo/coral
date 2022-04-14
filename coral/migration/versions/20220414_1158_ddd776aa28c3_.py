300359fb4a5ba0d1a6689a3eae5992e3c8e4c4d2
PatrickAdelberger <patrick.adelberger@jku.at>
2022-04-14 16:09:47 +0200

    fix flake8 linting errors

--- coral/migration/versions/20220414_1158_ddd776aa28c3_.py
+++ coral/migration/versions/20220414_1158_ddd776aa28c3_.py
@@ -17,29 +17,28 @@ depends_on = None
 def upgrade():
     connection = op.get_bind()
     for cmd in ["""
-        CREATE SCHEMA IF NOT EXISTS cohort;
-        CREATE SEQUENCE cohort.cohort_id_seq
-            INCREMENT 1
-            START 1
-            MINVALUE 1
-            MAXVALUE 2147483647
-            CACHE 1;
-        """, """
-        CREATE TABLE cohort.cohort
-        (
-            id integer NOT NULL DEFAULT nextval('cohort.cohort_id_seq'::regclass),
-            name character varying COLLATE pg_catalog."default" NOT NULL,
-            is_initial integer NOT NULL,
-            previous_cohort integer,
-            entity_database character varying COLLATE pg_catalog."default" NOT NULL,
-            entity_schema character varying COLLATE pg_catalog."default" NOT NULL,
-            entity_table character varying COLLATE pg_catalog."default" NOT NULL,
-            statement character varying COLLATE pg_catalog."default",
-            CONSTRAINT cohort_pkey PRIMARY KEY (id)
-        )
-        """]:
+CREATE SCHEMA IF NOT EXISTS cohort;
+CREATE SEQUENCE cohort.cohort_id_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 2147483647
+    CACHE 1;
+""", """
+CREATE TABLE cohort.cohort
+(
+    id integer NOT NULL DEFAULT nextval('cohort.cohort_id_seq'::regclass),
+    name character varying COLLATE pg_catalog."default" NOT NULL,
+    is_initial integer NOT NULL,
+    previous_cohort integer,
+    entity_database character varying COLLATE pg_catalog."default" NOT NULL,
+    entity_schema character varying COLLATE pg_catalog."default" NOT NULL,
+    entity_table character varying COLLATE pg_catalog."default" NOT NULL,
+    statement character varying COLLATE pg_catalog."default",
+    CONSTRAINT cohort_pkey PRIMARY KEY (id)
+);
+"""]:
         connection.execute(cmd)
-
 
 
 def downgrade():
