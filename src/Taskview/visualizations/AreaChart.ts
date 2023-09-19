5a5c7d2900bc07099f2e2dff113b2fbc2feae222
Richard Wolfmayr <richard.wolfmayr@gmail.com>
2023-09-19 10:27:41 +0200

    k-modes for 2 categorical attributes implemented

--- src/Taskview/visualizations/AreaChart.ts
+++ src/Taskview/visualizations/AreaChart.ts
@@ -5,6 +5,10 @@ import { ICohort } from '../../app/interfaces';
 import { DATA_LABEL } from './constants';
 import { MultiAttributeVisualization } from './MultiAttributeVisualization';
 import { IdValuePair } from '../../data/IAttribute';
+import {select} from "d3v7";
+import {createDBCohortAutomatically, ICohortMultiAttrDBDataParams} from "../../base";
+import {INewCohortDesc} from "../../util";
+import {AutoSplitEvent} from "../../base/events";
 
 export class AreaChart extends MultiAttributeVisualization {
   static readonly NAME = 'Area Chart';
@@ -13,8 +17,58 @@ export class AreaChart extends MultiAttributeVisualization {
     super(vegaLiteOptions);
   }
 
-  async createAutomatically() {
-    log.info('no automatic creation for Area Chart yet');
+  async createAutomatically(useNumberOfClusters: boolean = false) {
+    console.log("createAutomatically scatterplot");
+
+    // AttributeType = 'categorical' | 'number' | 'string'; TODO send it with the data
+
+    let numberOfClusters = 0;
+    if (useNumberOfClusters) {
+      // select the bins field
+      // binsCount = (this.controls.querySelector('#split input.bins') as HTMLInputElement).valueAsNumber;
+      numberOfClusters = (this.controls.querySelector(`#autoSplitControls input.clusters`) as HTMLInputElement).valueAsNumber;
+      console.log("numberOfClusters", numberOfClusters);
+    }
+
+    let newCohortIds = [];
+    let attributesMapped = this.attributes.map((attr) => {return {dataKey: attr.dataKey, type: attr.type}});
+    // convert the attributesParam to a JSON object
+    let attributesParam: string = JSON.stringify(attributesMapped);
+    for (const cht of this.cohorts) {
+      // const params: ICohortMultiAttrDBDataParams = {
+      //   cohortId: cht.dbId,
+      //   attribute0: this.attributes[0].dataKey,
+      //   attribute0type: this.attributes[0].type,
+      //   attribute1: this.attributes[1].dataKey,
+      //   attribute1type: this.attributes[1].type,
+      //   numberOfClusters: numberOfClusters,
+      // };
+
+      const params: ICohortMultiAttrDBDataParams = {
+        cohortId: cht.dbId,
+        attributes: attributesParam,
+        numberOfClusters: numberOfClusters,
+      };
+
+      newCohortIds = await createDBCohortAutomatically(params)
+      console.log("createAutomatically scatterplot data", newCohortIds);
+    }
+
+    let cohortDescs: INewCohortDesc[];
+    cohortDescs = [];
+    // for every selected cohort
+    for (const cohort of this.cohorts) {
+      // for every newCohort create a filter (for now... the filter is actually not needed, will be changed in the future)
+      for (const newCohort of newCohortIds){
+        cohortDescs.push({
+          cohort: cohort,
+          newCohortId: newCohort,
+          attr:[this.attributes[0], this.attributes[1]]
+        });
+      }
+    }
+
+    this.container.dispatchEvent(new AutoSplitEvent(cohortDescs));
   }
 
   getSpec(data: IdValuePair[]): VegaLiteSpec {
@@ -92,6 +146,99 @@ export class AreaChart extends MultiAttributeVisualization {
   }
 
   addControls() {
-    log.info('no controls for Area Chart yet');
+    // log.info('no controls for Area Chart yet');
+    this.controls.insertAdjacentHTML(
+      'afterbegin',
+      `
+    <div>
+      <!-- Nav tabs -->
+      <ul class="nav nav-tabs nav-justified" role="tablist">
+        <li role="presentation" class="nav-item"><a class="nav-link" href="#split" aria-controls="split" role="tab" data-bs-toggle="tab"><i class="fas fa-share-alt" aria-hidden="true"></i> Split</a></li>
+      </ul>
+      <!-- Tab panes -->
+      <div class="tab-content">
+        <div role="tabpanel" class="tab-pane" id="split">
+          <!-- INSERT SPLIT CONTROLS HERE -->
+        </div>
+      </div>
+    </div>
+    <div class="d-grid gap-2">
+      <div id="autoSplitControls">
+            <!-- INSERT autoSplitControls CONTROLS HERE -->
+            <div class="d-grid gap-2">
+              <button type="button" class="btn createAutomaticallyBtn btn-coral-prime" title="Calculate meaningful splits.">Create cohorts automatically</button>
+              <label>Number of Clusters</label>
+              <input type="number" class="clusters" step="any" min="1" max="99" value="2" />
+              <button type="button" class="btn createAutomaticallyWithNumberOfClustersBtn btn-coral-prime" title="Calculate meaningful splits.">Create cohorts for number of clusters</button>
+            </div>
+      </div>
+    </div>
+    `,
+    );
+
+
+
+
+
+    // for each attribute type, add the respective controls:
+    for (const [i, attr] of this.attributes.entries()) {
+      if (attr.type === 'number') {
+        const axis = i === 0 ? 'x' : 'y';
+        this.addIntervalControls(attr, axis);
+      }
+    }
+
+    select(this.controls)
+      .select('button.createAutomaticallyBtn')
+      .on('click', () => {
+        console.log("createAutomaticallyBtn clicked");
+        this.createAutomatically(false);
+      });
+
+    select(this.controls)
+      .select('button.createAutomaticallyWithNumberOfClustersBtn')
+      .on('click', () => {
+        console.log("createAutomaticallyWithNumberOfClustersBtn clicked");
+        this.createAutomatically(true);
+      });
+
+    select(this.controls)
+      .select('button.recommendSplitBtn')
+      .on('click', () => {
+        console.log("recommendSplitBtn clicked");
+        this.recommendSplit(false);
+      });
+
+    select(this.controls)
+      .select('button.recommendSplitWithNumberOfClustersBtn')
+      .on('click', () => {
+        console.log("recommendSplitWithNumberOfClustersBtn clicked");
+        this.recommendSplit(true);
+      });
+
+
+    select(this.controls)
+      .select('button.applyBtn')
+      .on('click', () => {
+        const activeTask = select(this.controls).select('.tab-pane.active').attr('id');
+        switch (activeTask) {
+          case 'filter':
+            this.filter();
+            break;
+          case 'split':
+            this.split();
+            break;
+          default:
+            log.error('Unknown task: ', activeTask);
+        }
+      });
+    const that = this; // helper varible to access this instance in the d3 event handler function
+    select(this.controls)
+      .selectAll('a[role="tab"]')
+      .on('click', function () {
+        const d3Event = this; // because we use a function this is overwritten by d3, asssign to variable for clarity
+        const newTask = (d3Event as HTMLAnchorElement).hash.replace('#', '') as 'filter' | 'split';
+        that.toggleFilterSplitMarks(newTask);
+      });
   }
 }
